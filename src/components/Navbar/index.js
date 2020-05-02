7280dedf243eaf27cd4781fa0bd9091d5f848c81
Ganesh Chittella <madhavi.0108@gmail.com>
2020-05-01 20:24:33 -0700

    resolved home page issue

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,82 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 const styles = {
   textColor: {
-   color : "white"
+    color: "white"
   }
 }
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-class Navbar extends React.Component {
-state = {
-  home: true
-}
+function Navbar() {
 
- linkClick = () => {
-    
-    this.setState({home : false});
-    console.log("home state is "+this.state.home);
+  const [isHomepage, setIsHomepage] = useState(true);
+  const linkClick = () => {
+    setIsHomepage(false);
   }
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-secondary p-0">
-        {!this.state.home && 
-        <Link className="navbar-brand ml-2 text-white" to="/" onClick={ () => this.setState({home: true})}>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary p-0">
+      {!isHomepage &&
+        <Link className="navbar-brand ml-2 text-white" to="/"
+          onClick={() => setIsHomepage(true)}
+        >
           Madhavi Mullapudi
-      </Link>}
-        <div className="ml-auto">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                to="/"
-                className={
-                  window.location.pathname === "/" ? "nav-link active" : "nav-link"
-                }
-                onClick={ () => this.setState({home: true})}
-                style={styles.textColor}
-              >
-                Home
-            </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-              onClick={this.linkClick}
-                to="/about"
-                className={window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-                }
-                style={styles.textColor}
-              >
-                About
-            </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/portfolio"
-                onClick={this.linkClick}
-                className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-                style={styles.textColor}
-              >
-                Portfolio
-            </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/contact"
-                onClick={this.linkClick}
-                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-                style={styles.textColor}
-              >
-                Contact
-            </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+        </Link>}
 
+      <div className="ml-auto">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" ? "nav-link active" : "nav-link"
+              }
+              onClick={() => setIsHomepage(true)}
+              style={styles.textColor}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              onClick={linkClick}
+              to="/about"
+              className={window.location.pathname === "/about"
+                ? "nav-link active"
+                : "nav-link"
+              }
+              style={styles.textColor}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/portfolio"
+              onClick={linkClick}
+              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              style={styles.textColor}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              onClick={linkClick}
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              style={styles.textColor}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 export default Navbar;
